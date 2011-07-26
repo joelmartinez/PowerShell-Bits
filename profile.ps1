990873087c6c65ce758d275d414dd0a59bdc4956
=Joel Martinez <joelmartinez@gmail.com>
2011-07-25 21:14:59 -0400

    Added 'sudo' function

--- profile.ps1
+++ profile.ps1
@@ -8,9 +8,21 @@ function prompt
 {
     $loc = $(get-location).ToString()
     $usr = ($env:userprofile).ToString()
-    if ($loc.StartsWith($usr, $true,[System.Globalization.CultureInfo].CurrentCulture)) {
+    if ($loc.StartsWith($usr, $true,[System.Globalization.CultureInfo].CurrentCulture))     {
 	$loc = "~"+$loc.SubString($usr.Length, $loc.Length - $usr.Length)
     }
     Write-Host ("PS " + $loc +">") -nonewline -foregroundcolor Yellow
     return " "
 }
+
+function elevate-process
+{
+    $file, [string]$arguments = $args;
+    $psi = new-object System.Diagnostics.ProcessStartInfo $file;
+    $psi.Arguments = $arguments;
+    $psi.Verb = "runas";
+    $psi.WorkingDirectory = get-location;
+    $runningProcess = [System.Diagnostics.Process]::Start($psi);
+}
+
+set-alias sudo elevate-process;
